220ea71958cc2a96ec02782e82d54e9d1ca1b032
Manthan Khandale <manthan@kisan.net>
2022-04-05 14:46:16 +0530

    ui changes

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   //Grey theme
-  // static final mainColor = Colors.grey[900]!;
-  // static final highlightColor = Colors.grey[850]!;
-  // static final shadowColor = Colors.black45;
-  // static final accentColor = Colors.grey[700]!;
+  static final mainColor = Colors.grey[900]!;
+  static final highlightColor = Colors.grey[850]!;
+  static final shadowColor = Colors.black45;
+  static final accentColor = Colors.grey[700]!;
 
   //Blue theme
-  static final mainColor = Color(0xff1A1A2E);
-  static final highlightColor = Colors.white10;
-  static final shadowColor = Colors.black45;
-  static final accentColor = Colors.grey;
+  // static final mainColor = Color(0xff1A1A2E);
+  // static final highlightColor = Colors.white10;
+  // static final shadowColor = Colors.black45;
+  // static final accentColor = Colors.grey;
 }
