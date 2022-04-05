499f462a28dc39556be3970d2e573f2fac1be878
Manthan Khandale <manthan@kisan.net>
2022-04-05 14:50:57 +0530

    red theme added

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   //Grey theme
-  static final mainColor = Colors.grey[900]!;
-  static final highlightColor = Colors.grey[850]!;
-  static final shadowColor = Colors.black45;
-  static final accentColor = Colors.grey[700]!;
+  // static final mainColor = Colors.grey[900]!;
+  // static final highlightColor = Colors.grey[850]!;
+  // static final shadowColor = Colors.black45;
+  // static final accentColor = Colors.grey[700]!;
 
   //Blue theme
   // static final mainColor = Color(0xff1A1A2E);
   // static final highlightColor = Colors.white10;
   // static final shadowColor = Colors.black45;
   // static final accentColor = Colors.grey;
+
+  //Red theme
+  static final mainColor = Color(0xffFD5D5D);
+  static final highlightColor = Color(0xffFF8080);
+  static final shadowColor = Colors.black45;
+  static final accentColor = Colors.white;
 }
